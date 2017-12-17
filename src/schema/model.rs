f6ce234a4bc92652fb861d030066179803ca66eb
Mcat12 <newtoncat12@yahoo.com>
2017-12-16 22:49:38 -0500

    Add FromId trait, implement Default for models, and remove Deserialize
    First step in removing auto-deserialization. It will be removed
    so that we can lazily load data from the database.
    
    Signed-off-by: Mcat12 <newtoncat12@yahoo.com>

--- src/schema/model.rs
+++ src/schema/model.rs
@@ -1,4 +1,7 @@
-#[derive(Deserialize)]
+pub trait FromId {
+    fn from_id(id: String) -> Self;
+}
+
 pub struct Album {
     pub id: String,
     pub artwork_url: Option<String>,
@@ -7,7 +10,26 @@ pub struct Album {
     pub songs: Vec<Song>
 }
 
-#[derive(Deserialize)]
+impl FromId for Album {
+    fn from_id(id: String) -> Album {
+        Album {
+            id, .. Album::default()
+        }
+    }
+}
+
+impl Default for Album {
+    fn default() -> Self {
+        Album {
+            id: "0".to_owned(),
+            artwork_url: None,
+            name: "".to_owned(),
+            artist: Artist::default(),
+            songs: vec![]
+        }
+    }
+}
+
 pub struct Artist {
     pub id: String,
     pub name: String,
@@ -16,7 +38,24 @@ pub struct Artist {
     pub singles: Vec<Album>
 }
 
-#[derive(Deserialize)]
+impl FromId for Artist {
+    fn from_id(id: String) -> Self {
+        Artist { id, .. Artist::default() }
+    }
+}
+
+impl Default for Artist {
+    fn default() -> Self {
+        Artist {
+            id: "0".to_owned(),
+            name: "".to_owned(),
+            albums: vec![],
+            featured: vec![],
+            singles: vec![]
+        }
+    }
+}
+
 pub struct Song {
     pub id: String,
     pub name: String,
@@ -28,7 +67,27 @@ pub struct Song {
     pub stats: SongUserStats
 }
 
-#[derive(Deserialize)]
+impl FromId for Song {
+    fn from_id(id: String) -> Self {
+        Song { id, .. Song::default() }
+    }
+}
+
+impl Default for Song {
+    fn default() -> Self {
+        Song {
+            id: "0".to_owned(),
+            name: "".to_owned(),
+            album: Album::default(),
+            artists: vec![],
+            stream_url: "".to_owned(),
+            track_number: 0,
+            disk_number: 0,
+            stats: SongUserStats::default()
+        }
+    }
+}
+
 pub struct SongUserStats {
     pub id: String,
     pub play_count: i32,
@@ -36,26 +95,54 @@ pub struct SongUserStats {
     pub liked: bool
 }
 
-// GraphQL binding located in resolvers
-#[derive(Deserialize)]
+impl FromId for SongUserStats {
+    fn from_id(id: String) -> Self {
+        SongUserStats { id, .. SongUserStats::default() }
+    }
+}
+
+impl Default for SongUserStats {
+    fn default() -> Self {
+        SongUserStats {
+            id: "0".to_owned(),
+            play_count: 0,
+            last_played: 0,
+            liked: false
+        }
+    }
+}
+
 pub struct Playlist {
     pub id: String,
     pub name: String,
 }
 
-#[derive(Deserialize)]
+impl FromId for Playlist {
+    fn from_id(id: String) -> Self {
+        Playlist { id, .. Playlist::default() }
+    }
+}
+
+impl Default for Playlist {
+    fn default() -> Self {
+        Playlist {
+            id: "0".to_owned(),
+            name: "".to_owned()
+        }
+    }
+}
+
 pub struct Edge<T> {
     pub cursor: String,
     pub node: T
 }
 
-#[derive(Deserialize)]
 pub struct Connection<T> {
     pub count: i32,
     pub edges: Vec<Edge<T>>
 }
 
-#[derive(GraphQLEnum, Deserialize)]
+#[derive(GraphQLEnum)]
 pub enum SortBy {
     #[graphql(
         name = "RECENTLY_ADDED",
