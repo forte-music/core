89844368625953367fffecfce7f138ad4c76f4aa
Mcat12 <newtoncat12@yahoo.com>
2018-01-12 13:01:21 -0500

    Take in references for adding structs in actions
    Signed-off-by: Mcat12 <newtoncat12@yahoo.com>

--- src/actions.rs
+++ src/actions.rs
@@ -7,16 +7,16 @@ fn add_list_to_set<T: Keyed>(set_id: &str, list: &[&str], db: &Connection) -> Re
     Ok(())
 }
 
-pub fn add_album(album: Album, db: &Connection) -> RedisResult<()> {
+pub fn add_album(album: &Album, db: &Connection) -> RedisResult<()> {
     db.hset_multiple::<_, _, _, ()>(Album::key(&album.id), &[
         ("id", &album.id),
-        ("artwork_url", &album.artwork_url.unwrap_or(String::new())),
+        ("artwork_url", &album.artwork_url.clone().unwrap_or(String::new())),
         ("name", &album.name),
         ("artist_id", &album.artist_id),
         ("release_year", &album.release_year.to_string())
     ])?;
 
-    db.sadd::<_, _, ()>("albums", album.id)?;
+    db.sadd::<_, _, ()>("albums", &album.id)?;
 
     Ok(())
 }
@@ -25,13 +25,13 @@ pub fn add_songs_to_album(album_id: &str, songs_ids: &[&str], db: &Connection) -
     add_list_to_set::<Album>(album_id, songs_ids, db)
 }
 
-pub fn add_artist(artist: Artist, db: &Connection) -> RedisResult<()> {
+pub fn add_artist(artist: &Artist, db: &Connection) -> RedisResult<()> {
     db.hset_multiple::<_, _, _, ()>(Artist::key(&artist.id), &[
         ("id", &artist.id),
         ("name", &artist.name)
     ])?;
 
-    db.sadd::<_, _, ()>("artists", artist.id)?;
+    db.sadd::<_, _, ()>("artists", &artist.id)?;
 
     Ok(())
 }
@@ -40,7 +40,7 @@ pub fn add_albums_to_artist(artist_id: &str, album_ids: &[&str], db: &Connection
     add_list_to_set::<Artist>(artist_id, album_ids, db)
 }
 
-pub fn add_song(song: Song, db: &Connection) -> RedisResult<()> {
+pub fn add_song(song: &Song, db: &Connection) -> RedisResult<()> {
     db.hset_multiple::<_, _, _, ()>(Song::key(&song.id), &[
         ("id", &song.id),
         ("name", &song.name),
@@ -52,7 +52,7 @@ pub fn add_song(song: Song, db: &Connection) -> RedisResult<()> {
         ("duration", &song.duration.to_string())
     ])?;
 
-    db.sadd::<_, _, ()>("songs", song.id)?;
+    db.sadd::<_, _, ()>("songs", &song.id)?;
 
     Ok(())
 }
@@ -61,7 +61,7 @@ pub fn add_artists_to_song(song_id: &str, artist_ids: &[&str], db: &Connection) 
     add_list_to_set::<Song>(song_id, artist_ids, db)
 }
 
-pub fn add_song_stats(song_stats: SongUserStats, db: &Connection) -> RedisResult<()> {
+pub fn add_song_stats(song_stats: &SongUserStats, db: &Connection) -> RedisResult<()> {
     db.hset_multiple::<_, _, _, ()>(SongUserStats::key(&song_stats.id), &[
         ("id", &song_stats.id),
         ("play_count", &song_stats.play_count.to_string()),
