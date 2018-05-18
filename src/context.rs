109cdc7428a505a27cd2fdb74f630371aed4fd65
Martin Charles <martincharles07@gmail.com>
2018-05-17 23:05:33 -0400

    Fixed Formatting

--- src/context.rs
+++ src/context.rs
@@ -7,8 +7,8 @@ use iron::prelude::*;
 use iron::typemap::Key;
 use juniper;
 use persistent::Read;
-use std::ops::Deref;
 use std::error::Error;
+use std::ops::Deref;
 
 pub type ConnectionManager = r2d2_diesel::ConnectionManager<SqliteConnection>;
 pub type Pool = r2d2::Pool<ConnectionManager>;
@@ -29,7 +29,7 @@ impl IronContext {
         let database_url = dotenv::var("DATABASE_URL")?;
         let manager = ConnectionManager::new(database_url);
         let pool: Pool = r2d2::Pool::new(manager)?;
-        let context = IronContext { pool: pool };
+        let context = IronContext { pool };
 
         Ok(Read::<ContextKey>::both(context))
     }
