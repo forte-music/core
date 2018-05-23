005d6b21e04aefe31e616cab0ce4c947979e668d
Martin Charles <martincharles07@gmail.com>
2018-05-23 16:36:34 -0400

    Added Import Module
    Given a SongProperties, tries to add it to the database. Should probably
    write some tests.
    
    Also added error chain to forte_core. It is only used in the new import
    code at the moment.

---
+++ src/import.rs
@@ -0,0 +1,124 @@
+use taglib2_sys::SongProperties;
+
+use chrono::Utc;
+use database::album;
+use database::artist;
+use diesel::associations::HasTable;
+use diesel::prelude::*;
+use diesel::result;
+use models::*;
+
+mod errors {
+    error_chain! {
+        foreign_links {
+            Diesel(::diesel::result::Error);
+        }
+
+        errors {
+            NoArtistError {
+                description("either the tag's album artist or artist needs to be set, neither is")
+            }
+
+            NoAlbumError {
+                description("the album name wasn't specified in the tag")
+            }
+
+            NoTitleError {
+                description("the title wasn't specified in the tag")
+            }
+        }
+    }
+}
+
+/// Takes information about a song read from tags and adds it to the database.
+pub fn add_song(props: SongProperties, conn: &SqliteConnection) -> errors::Result<()> {
+    let artist: Option<Artist> = props
+        .artist
+        .map_or(Ok(None), |name| add_or_get_artist(name, conn).map(Some))?;
+
+    let album_artist: Option<Artist> = props
+        .album_artist
+        .map_or(Ok(None), |name| add_or_get_artist(name, conn).map(Some))?;
+
+    let (artist, album_artist) = (
+        artist
+            .clone()
+            .or(album_artist.clone())
+            .ok_or(errors::ErrorKind::NoArtistError)?,
+        album_artist
+            .or(artist)
+            .ok_or(errors::ErrorKind::NoArtistError)?,
+    );
+
+    let album_name = props.album.ok_or(errors::ErrorKind::NoAlbumError)?;
+    let album = add_or_get_album(album_name, album_artist.id, props.year, conn)?;
+
+    let song = Song {
+        id: UUID::new(),
+        name: props.title.ok_or(errors::ErrorKind::NoTitleError)?,
+        album_id: album.id,
+        track_number: props.track_number as i32,
+        duration: props.duration,
+        time_added: Utc::now().naive_utc(),
+        play_count: 0,
+        last_played: None,
+        liked: false,
+    };
+
+    song.insert_into(Song::table()).execute(conn)?;
+
+    Ok(())
+}
+
+fn add_or_get_artist(name: String, conn: &SqliteConnection) -> Result<Artist, result::Error> {
+    let artist: Option<Artist> = artist::table
+        .filter(artist::name.eq(name.as_str()))
+        .first(conn)
+        .optional()?;
+
+    if let Some(artist) = artist {
+        return Ok(artist);
+    }
+
+    let artist = Artist {
+        id: UUID::new(),
+        name,
+        time_added: Utc::now().naive_utc(),
+        last_played: None,
+    };
+
+    artist.clone().insert_into(Artist::table()).execute(conn)?;
+
+    Ok(artist)
+}
+
+fn add_or_get_album(
+    name: String,
+    artist_id: UUID,
+    release_year: Option<u32>,
+    conn: &SqliteConnection,
+) -> Result<Album, result::Error> {
+    let album: Option<Album> = album::table
+        .filter(album::name.eq(name.as_str()))
+        .filter(album::artist_id.eq(artist_id))
+        .first(conn)
+        .optional()?;
+
+    if let Some(album) = album {
+        return Ok(album);
+    }
+
+    let album = Album {
+        id: UUID::new(),
+        artwork_url: None,
+        name,
+        artist_id,
+        release_year: release_year.map(|year| year as i32),
+        time_added: Utc::now().naive_utc(),
+        last_played: None,
+    };
+
+    album.clone().insert_into(Album::table()).execute(conn)?;
+
+    Ok(album)
+}
--- src/lib.rs
+++ src/lib.rs
@@ -10,11 +10,15 @@ extern crate uuid;
 
 #[macro_use]
 extern crate juniper;
-
 #[macro_use]
 extern crate diesel;
+#[macro_use]
+extern crate error_chain;
+
+extern crate taglib2_sys;
 
 pub mod context;
 pub mod database;
+pub mod import;
 pub mod models;
 pub mod server;
