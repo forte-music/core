6eba1b11307c4bf9598c3c2fe04fe52eebd0a365
Martin Charles <martincharles07@gmail.com>
2018-05-24 00:14:32 -0400

    Added Progress Bar

--- src/bin/sync/main.rs
+++ src/bin/sync/main.rs
@@ -1,5 +1,6 @@
 extern crate dotenv;
 extern crate forte_core;
+extern crate indicatif;
 extern crate r2d2;
 extern crate taglib2_sys;
 extern crate walkdir;
@@ -11,12 +12,15 @@ use std::env;
 use std::ops::Deref;
 
 use dotenv::dotenv;
+use indicatif::ProgressBar;
 use walkdir::WalkDir;
 
 use forte_core::context;
 use forte_core::import;
 
+use indicatif::ProgressStyle;
 use taglib2_sys::SongProperties;
+use walkdir::DirEntry;
 
 error_chain! {
     links {
@@ -51,22 +55,30 @@ fn start() -> Result<()> {
     let conn = connection.deref();
 
     let path = args.get(0).unwrap();
-    let iter = WalkDir::new(path).follow_links(true);
+    let entries: Vec<DirEntry> = WalkDir::new(path)
+        .follow_links(true)
+        .into_iter()
+        .filter_map(|d| d.ok())
+        .filter(|entry| {
+            let path = entry.path();
+            let extension = path.extension().and_then(|e| e.to_str());
+            match extension {
+                Some(extension) if FORMAT_EXTENSIONS.contains(&extension) => true,
+                _ => false,
+            }
+        })
+        .collect();
 
-    for entry in iter {
-        let entry = entry?;
-        let entry_path = entry.path();
-        let extension = if let Some(extension) = entry_path.extension().and_then(|e| e.to_str()) {
-            extension
-        } else {
-            continue;
-        };
+    let bar = ProgressBar::new(entries.len() as u64);
+    bar.set_style(
+        ProgressStyle::default_bar()
+            .template("[{elapsed_precise}] [{bar:40.cyan/blue}] {pos:>7}/{len:7} ({eta})")
+            .progress_chars("#>-"),
+    );
 
-        if !FORMAT_EXTENSIONS.contains(&extension) {
-            continue;
-        }
-
-        let props = SongProperties::read(entry_path)?;
+    for entry in entries {
+        let path = entry.path();
+        let props = SongProperties::read(path)?;
         let props = if let Some(props) = props {
             props
         } else {
@@ -74,7 +86,11 @@ fn start() -> Result<()> {
         };
 
         import::add_song(props, conn)?;
+
+        bar.inc(1);
     }
+
+    bar.finish();
 
     Ok(())
 }
