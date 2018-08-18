a807ffb9a2119b30e13b44e33bdf883f2ccb46a8
Martin Charles <martincharles07@gmail.com>
2018-08-18 00:19:16 -0400

    Removed expect In Transcoding Handler

--- src/bin/server/transcoding.rs
+++ src/bin/server/transcoding.rs
@@ -7,25 +7,21 @@ use actix_web::State;
 use forte_core::models::Song;
 
 use server::graphql::AppState;
+use server::stream::RangeStream;
 use server::transcoder::{TranscodeMessage, TranscodeTarget};
 
 use diesel;
 
 use uuid::Uuid;
 
+use futures::future;
 use futures::Future;
 
 use lru_disk_cache::ReadSeek;
-use server::stream::RangeStream;
+
+use std::io;
 use std::io::SeekFrom;
 use std::path::PathBuf;
-
-fn convert_diesel_err(err: diesel::result::Error) -> actix_web::Error {
-    match err {
-        diesel::result::Error::NotFound => error::ErrorNotFound(err),
-        _ => error::ErrorInternalServerError(err),
-    }
-}
 
 pub fn handler(
     state: State<AppState>,
@@ -33,32 +29,54 @@ pub fn handler(
 ) -> actix_web::FutureResponse<RangeStream<Box<ReadSeek>>> {
     let song_id = song_id.into_inner();
 
+    future::done(build_transcode_message(song_id, &state))
+        .and_then(move |transcode_msg| {
+            state
+                .transcoder
+                .send(transcode_msg)
+                .map_err(error::ErrorInternalServerError)
+        })
+        .and_then(|result| {
+            result.map_err(|e| error::ErrorInternalServerError(e.description().to_string()))
+        })
+        .and_then(|mut reader| {
+            get_size(&mut reader)
+                .map(|size| (reader, size))
+                .map_err(error::ErrorInternalServerError)
+        })
+        .map(|(reader, size)| RangeStream::new(reader, size))
+        .responder()
+}
+
+fn build_transcode_message(
+    song_id: Uuid,
+    state: &State<AppState>,
+) -> actix_web::Result<TranscodeMessage<PathBuf>> {
     let context = state
         .build_context()
-        .map_err(error::ErrorInternalServerError)
-        .expect("// TODO: Remove");
+        .map_err(error::ErrorInternalServerError)?;
 
-    let song = Song::from_id(&context, &song_id.into())
-        .map_err(convert_diesel_err)
-        .expect("// TODO: Remove");
+    let song = Song::from_id(&context, &song_id.into()).map_err(convert_diesel_err)?;
 
     let song_path: PathBuf = song.path.into();
 
-    state
-        .transcoder
-        .send(TranscodeMessage::new(
-            song_path,
-            song_id.to_string(),
-            TranscodeTarget::MP3V0,
-        ))
-        .map(|f| {
-            let mut reader = f.expect("// TODO: Remove");
+    Ok(TranscodeMessage::new(
+        song_path,
+        song_id.to_string(),
+        TranscodeTarget::MP3V0,
+    ))
+}
 
-            let size = reader.seek(SeekFrom::End(0)).expect("// TODO: Remove");
-            reader.seek(SeekFrom::Start(0)).expect("// TODO: Remove");
+fn get_size<R: ReadSeek>(reader: &mut R) -> io::Result<u64> {
+    let size = reader.seek(SeekFrom::End(0))?;
+    reader.seek(SeekFrom::Start(0))?;
 
-            RangeStream::new(reader, size)
-        })
-        .map_err(|e| error::ErrorInternalServerError(e))
-        .responder()
+    Ok(size)
+}
+
+fn convert_diesel_err(err: diesel::result::Error) -> actix_web::Error {
+    match err {
+        diesel::result::Error::NotFound => error::ErrorNotFound(err),
+        _ => error::ErrorInternalServerError(err),
+    }
 }
