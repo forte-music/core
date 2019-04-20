764056b6ceebb182bd34c161f60a57c851ce5531
Martin Charles <martincharles07@gmail.com>
2019-04-20 12:18:30 -0700

    Removed IronContext

--- src/context.rs
+++ src/context.rs
@@ -10,10 +10,6 @@ pub type ConnectionManager = r2d2_diesel::ConnectionManager<SqliteConnection>;
 pub type Pool = r2d2::Pool<ConnectionManager>;
 pub type PooledConnection = r2d2::PooledConnection<ConnectionManager>;
 
-pub struct IronContext {
-    pub pool: Pool,
-}
-
 pub fn init_pool(database_url: &str) -> Result<Pool, r2d2::Error> {
     let manager = ConnectionManager::new(database_url);
     r2d2::Pool::new(manager)
@@ -27,9 +23,7 @@ impl GraphQLContext {
     pub fn new(connection: PooledConnection) -> GraphQLContext {
         GraphQLContext { connection }
     }
-}
 
-impl GraphQLContext {
     pub fn connection(&self) -> &SqliteConnection {
         self.connection.deref()
     }
